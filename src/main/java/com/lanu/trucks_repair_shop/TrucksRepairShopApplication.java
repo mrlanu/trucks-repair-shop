1b4a7bda34cb111493a965b65a3d4b7f2658bf35
Serhiy K <mrlanu@gmail.com>
2018-03-05 20:50:24 -0600

    init commit

---
+++ src/main/java/com/lanu/trucks_repair_shop/TrucksRepairShopApplication.java
@@ -0,0 +1,12 @@
+package com.lanu.trucks_repair_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrucksRepairShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrucksRepairShopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lanu/trucks_repair_shop/TrucksRepairShopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lanu.trucks_repair_shop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrucksRepairShopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
