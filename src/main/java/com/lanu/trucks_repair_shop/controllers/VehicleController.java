07f584fdea1607812cced709a31a072ed41f621b
Serhiy K <mrlanu@gmail.com>
2018-03-08 20:59:55 -0600

    truck.html trailer.html

--- src/main/java/com/lanu/trucks_repair_shop/controllers/VehicleController.java
+++ src/main/java/com/lanu/trucks_repair_shop/controllers/VehicleController.java
@@ -1,5 +1,7 @@
 package com.lanu.trucks_repair_shop.controllers;
 
+import com.lanu.trucks_repair_shop.entities.Trailer;
+import com.lanu.trucks_repair_shop.entities.Truck;
 import com.lanu.trucks_repair_shop.services.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,5 +19,18 @@ public class VehicleController {
     public String unitsList(Model model){
         model.addAttribute("vehiclesList", vehicleService.findAll());
         return "vehiclesList";
+    }
+
+    @GetMapping("/showVehicle")
+    public String showVehicle(@RequestParam("type") String type, @RequestParam("number") Integer number, Model model){
+        if (type.equalsIgnoreCase("Truck")){
+            Truck truck = (Truck) vehicleService.findByNumber(number);
+            model.addAttribute("vehicle", truck);
+            return "/truck";
+        }else {
+            Trailer trailer = (Trailer) vehicleService.findByNumber(number);
+            model.addAttribute("vehicle", trailer);
+            return "/trailer";
+        }
     }
 }
