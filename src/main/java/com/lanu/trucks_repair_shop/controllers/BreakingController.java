51a925eb5bf20fe0969c18b8f90744cc8c25543c
Serhiy K <mrlanu@gmail.com>
2018-03-15 23:49:33 -0500

    breakingList: html, controller, style

---
+++ src/main/java/com/lanu/trucks_repair_shop/controllers/BreakingController.java
@@ -0,0 +1,25 @@
+package com.lanu.trucks_repair_shop.controllers;
+
+import com.lanu.trucks_repair_shop.entities.Vehicle;
+import com.lanu.trucks_repair_shop.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/breaking")
+public class BreakingController {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @GetMapping("/breakingList")
+    public String breakingListt(@RequestParam("number") Integer number, @RequestParam("type") String type, Model model){
+        Vehicle vehicle = vehicleService.findByNumber(number);
+        model.addAttribute("vehicle", vehicle);
+        return "breaking/breakingList";
+    }
+}
