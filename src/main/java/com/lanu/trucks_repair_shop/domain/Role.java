e8728c8c1099bb46ed2c8ada3536be4ef207d0d4
Serhiy K <mrlanu@gmail.com>
2018-03-16 22:25:48 -0500

    Role Class

--- src/main/java/com/lanu/trucks_repair_shop/domain/Role.java
+++ src/main/java/com/lanu/trucks_repair_shop/domain/Role.java
@@ -1,4 +1,47 @@
 package com.lanu.trucks_repair_shop.domain;
 
-public class Role {
+import javax.persistence.FetchType;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Role extends AbstractDomainClass {
+
+    private String role;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable
+    private List<User> users = new ArrayList<>();
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public void addUser(User user){
+        if(!this.users.contains(user)){
+            this.users.add(user);
+        }
+
+        if(!user.getRoles().contains(this)){
+            user.getRoles().add(this);
+        }
+    }
+
+    public void removeUser(User user){
+        this.users.remove(user);
+        user.getRoles().remove(this);
+    }
 }
