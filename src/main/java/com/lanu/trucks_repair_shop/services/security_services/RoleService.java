21501c3fbf696e1688e500b7741e71d34b9c1050
Serhiy K <mrlanu@gmail.com>
2018-03-17 22:43:20 -0500

    done

---
+++ src/main/java/com/lanu/trucks_repair_shop/services/security_services/RoleService.java
@@ -0,0 +1,7 @@
+package com.lanu.trucks_repair_shop.services.security_services;
+
+import com.lanu.trucks_repair_shop.domain.security.Role;
+import com.lanu.trucks_repair_shop.services.security_services.CRUDService;
+
+public interface RoleService extends CRUDService<Role> {
+}
---
+++ src/main/java/com/lanu/trucks_repair_shop/services/services_impl/security_services_impl/RoleServiceImpl.java
@@ -0,0 +1,39 @@
+package com.lanu.trucks_repair_shop.services.services_impl.security_services_impl;
+
+import com.lanu.trucks_repair_shop.domain.security.Role;
+import com.lanu.trucks_repair_shop.repositories.security_repositories.RoleRepository;
+import com.lanu.trucks_repair_shop.services.security_services.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+    @Service
+    public class RoleServiceImpl implements RoleService {
+
+        @Autowired
+        private RoleRepository roleRepository;
+
+        @Override
+        public List<?> listAll() {
+            List<Role> roles = new ArrayList<>();
+            roleRepository.findAll().forEach(roles::add);
+            return roles;
+        }
+
+        @Override
+        public Role getById(Integer id) {
+            return roleRepository.findOne(id);
+        }
+
+        @Override
+        public Role saveOrUpdate(Role domainObject) {
+            return roleRepository.save(domainObject);
+        }
+
+        @Override
+        public void delete(Integer id) {
+            roleRepository.delete(id);
+        }
+    }
