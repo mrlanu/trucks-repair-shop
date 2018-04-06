19b63080b95f25f880009f4069f3ceda39ad70ab
Serhiy K <mrlanu@gmail.com>
2018-04-05 20:41:26 -0500

    modal form for confirm delete vehicle

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -17,12 +17,39 @@
             }
         })
     };
+
+    /*$.enabledDescription = function () {
+        var control, i = 1;
+
+            control = $("#id_check_" + i);
+            control.bind('click', function () {
+                if (control.is(':checked')) {
+                    document.getElementById("id_desc_" + i).readOnly = false;
+
+                } else {
+                    document.getElementById("id_desc_" + i).readOnly = true;
+                    $('#id_desc_' + i).val(null);
+
+                }
+            })
+
+    };*/
+
 }(jQuery));
 
 $(document).ready(function() {
     $.toggleShowPassword({
         field: '#password',
         control: "#showPassword"
+    });
+
+    /*$.enabledDescription();*/
+
+    $('.table .delBtn').on('click',function(event){
+        event.preventDefault();
+        var href = $(this).attr('href');
+        $('#myModal #delRef').attr('href',href);
+        $('#myModal').modal();
     });
 });
 
