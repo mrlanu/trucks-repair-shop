4c1dbedca3ba47b6a23ad5dcc79f1c7e21eaca77
Serhiy K <mrlanu@gmail.com>
2018-04-12 14:54:51 -0500

    method init()will start just on H2 DB

--- src/main/java/com/lanu/trucks_repair_shop/TrucksRepairShopApplication.java
+++ src/main/java/com/lanu/trucks_repair_shop/TrucksRepairShopApplication.java
@@ -12,6 +12,7 @@ import com.lanu.trucks_repair_shop.repositories.MakeRepository;
 import com.lanu.trucks_repair_shop.services.security_services.UserService;
 import com.lanu.trucks_repair_shop.services.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -30,13 +31,21 @@ public class TrucksRepairShopApplication implements CommandLineRunner{
 	@Autowired
 	private MakeRepository makeRepository;
 
+	@Value("${spring.datasource.url}")
+	private String springDatasourceUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(TrucksRepairShopApplication.class, args);
 	}
 
 	@Override
 	public void run(String... strings) throws Exception {
-		init();
+
+		//when running app on the localhost H2 DB the init() method will run
+
+		String localDBUrlH2 = "jdbc:h2:mem:testdb";
+
+		if (springDatasourceUrl.equals(springDatasourceUrl.equals(localDBUrlH2))){init();}
 	}
 
 	private void init(){
