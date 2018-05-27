e9c41eeade0d5034ac3f7de3cc06c5a383fb86ae
Serhiy K <mrlanu@gmail.com>
2018-05-26 20:31:58 -0500

    Test @RestController

---
+++ src/main/java/com/lanu/trucks_repair_shop/controllers/TestController.java
@@ -0,0 +1,21 @@
+package com.lanu.trucks_repair_shop.controllers;
+
+import com.lanu.trucks_repair_shop.domain.vehicle.Vehicle;
+import com.lanu.trucks_repair_shop.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @GetMapping("/test")
+    public Vehicle getTest(){
+        return vehicleService.findByNumber(775);
+    }
+}
